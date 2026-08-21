c17c4f76882a6f73aacf0cbde7282e0a9c179415
sskrishnan03 <sskrishnan03@gmail.com>
2026-08-21 20:00:16 +0530

    Updated

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,3 @@
-import { jsxLocPlugin } from "@builder.io/vite-plugin-jsx-loc";
 import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
 import fs from "node:fs";
@@ -152,7 +151,6 @@ function vitePluginKsemoDebugCollector(): Plugin {
 const plugins = [
   react(),
   tailwindcss(),
-  jsxLocPlugin(),
   vitePluginKsemoDebugCollector(),
 ];
 
