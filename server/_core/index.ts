7c068c1bf45b12ab17b598f8cc9330ebe7f58e39
sskrishnan03 <sskrishnan03@gmail.com>
2026-09-12 09:58:09 +0530

    update

--- server/_core/index.ts
+++ server/_core/index.ts
@@ -68,7 +68,7 @@ async function startServer() {
     serveStatic(app);
   }
 
-  const port = 3000;
+  const port = process.env.PORT ? parseInt(process.env.PORT, 10) : 3000;
   server.listen(port, "0.0.0.0", () => {
     console.log(`\n  ➜  Local:   http://localhost:${port}/\n`);
   });
